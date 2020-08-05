1956861fb7dff99c87455e5166ed739110b5213e
Vadim <vadim.kurn@gmail.com>
2020-08-05 21:11:53 +0300

    Bonus.java edited again

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,20 +1,20 @@
 public class Bonus {
     public static void main(String[] args) {
         int clientAccountInRoubles = 100;
-        int depositSum = 1100;
+        int depositSumInRoubles = 1100;
         int bonusRate = 100;
         int bonus;
         int total;
-        if (depositSum >= 1000) {
-            bonus = depositSum / bonusRate;
-            total = clientAccountInRoubles + depositSum + bonus;
+        if (depositSumInRoubles >= 1000) {
+            bonus = depositSumInRoubles / bonusRate;
+            total = clientAccountInRoubles + depositSumInRoubles + bonus;
         }
         else {
             bonus = 0;
-            total = clientAccountInRoubles + depositSum;
+            total = clientAccountInRoubles + depositSumInRoubles;
         }
         System.out.println("Текущий баланс: " + clientAccountInRoubles + "₽");
-        System.out.println("Будет зачислено: " + depositSum + "₽");
+        System.out.println("Будет зачислено: " + depositSumInRoubles + "₽");
         System.out.println("Бонус от зачисления: " + bonus + "₽");
         System.out.println("Баланс после зачисления: " + total + "₽");
     }
